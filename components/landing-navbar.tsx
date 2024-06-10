5218233705f2c0384e44c6dd7667e629741b6dcf
charbelyou444 <123186304+charbelyou444@users.noreply.github.com>
2024-06-10 18:37:27 +0300

    Update landing-navbar.tsx

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -26,7 +26,8 @@ export const LandingNavbar = () => {
 
                     </div>
                     <h1 className ={cn("text-2xl ,font-bold text-white",font.className)}>
-                        Priapus 
+                        Charbel Youssef
+                        
                     </h1>
                 </Link>
                 <div className="flex items-center gap-x-2">
