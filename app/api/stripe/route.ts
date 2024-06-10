bf79e68b7ae4bae3ea164ad8234be5221f9e4fdd
charbelyou444 <123186304+charbelyou444@users.noreply.github.com>
2024-06-10 18:42:56 +0300

    Update route.ts

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -40,8 +40,7 @@ export async function GET(){
                             price_data:{
                                 currency:"USD",
                                 product_data:{
-                                    name:"Charbel Youssef Pro
-                                        "
+                                    name:"Charbel Youssef Pro"
                                 ,description:"Unlimited AI Generations ",
                             },
                             unit_amount:2000,
